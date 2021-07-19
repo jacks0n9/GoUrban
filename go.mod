3d40b946b39a896e2210c184c6cae04676330b98
jacks0n9 <cat47info@gmail.com>
2021-07-19 16:34:45 -0700

    Add comments

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jacks0n9/gourban
 
 go 1.16
+
+require golang.org/x/tools v0.1.5 // indirect
--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 	"net/http"
 )
 
+// An array of definitions
 type Definitions []Definition
+
+// A struct representing a definition definition.
+//UpVotes are known as thumbs_up on the website, but UpVotes was shorter. Same with thumbs_down.
+//current_vote was omitted because it never has a value.
 type Definition struct {
 	Def       string   `json:"definition"`
 	Permalink string   `json:"permalink"`
@@ -21,6 +26,7 @@ type Definition struct {
 	Example   string   `json:"example"`
 }
 
+// Usage: Define(term). This will query the urban dictionary api for your term.
 func Define(term string) Definitions {
 	resp, err := http.Get("https://api.urbandictionary.com/v0/define?term=" + term)
 	body, _ := io.ReadAll(resp.Body)
